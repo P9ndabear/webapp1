81d035b8f4e8ba5009ea361cc491ed1060967745
P9ndabear <1208846@student.roc-nijmegen.nl>
2024-05-15 11:16:15 +0200

    rowCount toegevoegd

--- searchbar.php
+++ searchbar.php
@@ -1,22 +1,26 @@
 <?php require_once "includes/connect.php" ?>
 
 
-<?php 
-    if (isset($_GET['submit-search'])) {
-        $search = $_GET['search'];
-        $sql = "SELECT * FROM gerechten WHERE naam LIKE :search";
-        $stmt = $connection->prepare($sql);
-        $var = "%" . $_GET['search'] . "%";
-        $stmt->bindParam(":search",  $var);
+<?php
+if (isset($_GET['submit-search'])) {
+    $search = $_GET['search'];
+    $sql = "SELECT * FROM gerechten WHERE naam LIKE :search";
+    $stmt = $connection->prepare($sql);
+    $var = "%" . $_GET['search'] . "%";
+    $stmt->bindParam(":search",  $var);
 
-        $stmt->execute();
+    $stmt->execute();
 
-        
-            while ($gerechten = $stmt->fetch()) {            
-            echo "<div class='menu-container'>" .   
-            "<div class='naam'>" . $gerechten["naam"] . "</div>";
-        echo "<div class='omschrijving'>" . $gerechten["omschrijving"] . "</div>";
-        echo "</div>";
+
+    while ($gerechten = $stmt->fetch()) {
+        if ($stmt->rowCount() > 0) {
+            echo "<div class='menu-container'>" .
+                "<div class='naam'>" . $gerechten["naam"] . "</div>";
+            echo "<div class='omschrijving'>" . $gerechten["omschrijving"] . "</div>";
+            echo "</div>";
         }
     }
+} if ($stmt->rowCount() < 1) {
+    echo "Geen resultaten gevonden";
+}
 ?>
